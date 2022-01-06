ba57beb4c73a3ad00add36960c1ddc057e1fbde2
Bach Chau <bchau3@nait.ca>
2022-01-06 16:10:16 -0700

    first commit

--- SimpleApp/Program.cs
+++ SimpleApp/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Can you C#?");
+Console.Write("What is the different bettween Write and WriteLine");
